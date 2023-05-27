4bec51c6e92ecdbe913bb63432f12df5d5ce76eb
NurfaGazman <nurfathihahgazman@gmail.com>
2023-05-27 14:42:58 +0800

    server

---
+++ src/main/java/com/PSM/B032110450/Ladies_Safety_Life_Server/LadiesSafetyLifeServerApplication.java
@@ -0,0 +1,13 @@
+package com.PSM.B032110450.Ladies_Safety_Life_Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LadiesSafetyLifeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LadiesSafetyLifeServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PSM/B032110450/Ladies_Safety_Life_Server/LadiesSafetyLifeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PSM.B032110450.Ladies_Safety_Life_Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LadiesSafetyLifeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
